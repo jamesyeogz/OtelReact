97102e38d99c38bc3a433d0316741e41fe844044
James <yeo.gz@csit.gov.sg>
2025-08-31 23:15:10 +0800

    fix: added _startSpan

--- src/instrumentation/ReactOpenTelemetry.ts
+++ src/instrumentation/ReactOpenTelemetry.ts
@@ -1,7 +1,13 @@
 import { InstrumentationBase, InstrumentationConfig } from '@opentelemetry/instrumentation';
-import * as api from '@opentelemetry/api';
-import { SpanData } from './internal-types';
-import { WindowWithZone, ZoneTypeWithPrototype } from './internal-types';
+import {
+    context,
+    propagation,
+    trace,
+    Span,
+    ROOT_CONTEXT,
+    Tracer,
+  } from '@opentelemetry/api';
+
 export interface ReactOpenTelemetryHookConfig extends InstrumentationConfig {
   // No additional config needed for basic implementation
 }
@@ -13,7 +19,6 @@ export interface ReactOpenTelemetryHookConfig extends InstrumentationConfig {
 export class ReactOpenTelemetryHook extends InstrumentationBase<ReactOpenTelemetryHookConfig> {
     readonly version = "0.0.1";
     readonly moduleName: string = 'user-interaction';
-    private _spansData = new WeakMap<api.Span, SpanData>();
   constructor(config: ReactOpenTelemetryHookConfig = {}) {
     super('@jyeogz/opentelemetry-react-hook', '1.0.0', config);
   }
@@ -32,16 +37,32 @@ export class ReactOpenTelemetryHook extends InstrumentationBase<ReactOpenTelemet
     // Disable the instrumentation
     this._diag.debug('ReactOpenTelemetryHook disabled');
   }
+  private _startSpan(
+    spanName: string,
+    parentSpan?: Span,
+    attributes?: Record<string, string | number | boolean>
+  ): Span | undefined {
+      const span = this.tracer.startSpan(
+        spanName,
+        {
+            ...attributes
+        },
+        parentSpan ? trace.setSpan(context.active(), parentSpan) : undefined
+      );
+      return span;
+    
+    return undefined;
+  }
+
+  private _endSpan(span: Span, success?: boolean, errorMessage?: string) {
+    span.end();
+  }
 
   /**
    * Get the tracer instance for this instrumentation
    */
-  getTracer(): api.Tracer {
+  getTracer(): Tracer {
     return this.tracer;
   }
 
-  private _getZoneWithPrototype(): ZoneTypeWithPrototype | undefined {
-    const _window: WindowWithZone = window as unknown as WindowWithZone;
-    return _window.Zone;
-  }
 }
